8025c88c989d002712bf322a17890faaeca495fe
Song WonJoon <wonjoon0730@gmail.com>
2021-06-24 13:55:11 +0900

    :memo: posting model update

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const userRouter = require('./routers/user');
 const postRouter = require('./routers/post');
 const mongoose = require('mongoose');
 const { User } = require('./models/User');
+const { Posting } = require('./models/Posting');
 
 
 //MongoDB
@@ -43,8 +44,7 @@ app.get('/', (req,res) => {
     console.log('연결 성공')
     res.send('Hello SENNA!!!')
 })
-
-//
+//User post
 app.post("/register", (req, res) => {
 
     // User 데이터 모델과 연결된 객체 생성 후 req.body 삽입
@@ -56,6 +56,20 @@ app.post("/register", (req, res) => {
       if (err) return res.json({ suceess: false, err });
       // 성공적이면 200 상태 코드 날리고 true 값 돌려주기
       return res.status(200).json({ suceess: true });
+    });
+  });
+//Posting post
+  app.post("/regi", (req, res) => {
+
+    // User 데이터 모델과 연결된 객체 생성 후 req.body 삽입
+    const posting = new Posting(req.body);
+  
+    // save 메서드를 통해 원격 저장소에 데이터 저장.
+    posting.save((err, posting) => {
+      // 에러면 false 반환
+      if (err) return res.json({ success: false, err });
+      // 성공적이면 200 상태 코드 날리고 true 값 돌려주기
+      return res.status(200).json({ success: true });
     });
   });
 

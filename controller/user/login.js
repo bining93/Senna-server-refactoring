f8545214dfa82bc9ac18727371f4d1dabab5b880
Song WonJoon <wonjoon0730@gmail.com>
2021-06-28 15:35:53 +0900

    :ambulance: login fixed

--- controller/user/login.js
+++ controller/user/login.js
@@ -3,44 +3,44 @@ import jwt from 'jsonwebtoken';
 
 const login = async (req, res) => {
 
-  const { userId, password } = req.body
+  const { userId, password } = req.body;
+//   const Users = mongoose.model('User', User.userSchema);
+  await User.findOne({
+    userId: userId,
+    password: password,
+  }).then((data) => {
+      if(!data) {
+        res.status(401).send('아이디와 비밀번호를 확인해주세요');
+      }else if(data.status === false) {
+        res.status(404).send('존재하지 않는 유저입니다');
+      } else {
+        // 일치하는 유저가 있을 경우
+        // access token, refresh token 두가지를 생성
+        const { userId, profileImage, favorite } = data;
+        const accessToken = jwt.sign({ userId, profileImage, favorite }, process.env.TOKEN_SECRET, {
+          expiresIn: '1h',
+        });
+        // const refreshToken = jwt.sign({ userid, profileImage, favorite }, process.env.REFRESH_SECRET, {
+        //   expiresIn: '10h',
+        // });
+        // // 생성된 refresh token을 쿠키에 담아줍니다
+        // res.cookie('refreshToken', refreshToken, {
+        //   sameSite: 'none',
+        //   secure: true, 
+        //   httpOnly: true
+        // });
+        
+        res.status(200).send({
+          data: {
+            accessToken
+          }, 
+          message: 'accessToken'
+        });
+      }
 
-  const userInfo = await User.findOne({
-    where: { 
-      userId: userId,
-      password: password,
-    }
-  });
+  })
 
-  console.log('userInfo', userInfo)
-  if (!userInfo) {
-    // 일치하는 유저가 없을 경우
-    res.status(401).send('아이디와 비밀번호를 확인해주세요');
-  } else if(!userInfo.status) {
-    res.status(404).send('존재하지 않는 유저입니다');
-  } else {
-    // 일치하는 유저가 있을 경우
-    // access token, refresh token 두가지를 생성
-    const { userid, profileImage, favorite } = userInfo;
-    const accessToken = jwt.sign({ userid, profileImage, favorite }, process.env.ACCESS_SECRET, {
-      expiresIn: '1h',
-    });
-    // const refreshToken = jwt.sign({ userid, profileImage, favorite }, process.env.REFRESH_SECRET, {
-    //   expiresIn: '10h',
-    // });
-    // // 생성된 refresh token을 쿠키에 담아줍니다
-    // res.cookie('refreshToken', refreshToken, {
-    //   sameSite: 'none',
-    //   secure: true, 
-    //   httpOnly: true
-    // });
-    res.status(200).send({
-      data: {
-        accessToken
-      }, 
-      message: 'accessToken'
-    });
-  }
+  
 
 }
 
--- models/User.js
+++ models/User.js
@@ -3,22 +3,30 @@ import mongoose from "mongoose";
   const userSchema = mongoose.Schema({
     userId: {
         type: String,
+        required: true,
+        unique: true,
         maxLength: 50,
     },
     password: {
         type: String,
+        required: true,
         minlength: 5,
-    },
-    profileImage: {
-        type: String,
     },
     favorite: {
         type: Array,
     },
+    profileImage: {
+        type: String,
+    },
     status: {
         type: Boolean,
+        default: true,
     }
 })
+// userSchema.methods.dudify = () => {
+//     this.name = this.name + '-dude';
+//     return this.name;
+// }
 const User = mongoose.model("User", userSchema);
 // 모델의 이름과 스키마를 이용해 모델의 정의함.
 export default User;

cb0504a2e7107c5838822873c32820f37bf55489
hyebin <dlgpqls93@gmail.com>
2021-06-29 17:50:18 +0900

    :memo: update upload

--- controller/post/modify.js
+++ controller/post/modify.js
@@ -4,15 +4,22 @@ import Posting from "../../models/Posting.js";
 const modify = async (req, res) => {
     //postingId 값이 params에 담겨져 온다. 
     //console.log('req', req.params.id)
-    const { userId, image, content } = req.body;
+    const { userId, content } = req.body;
     const postingId = req.params.id;
+    const images = req.files;
+    const path = images.map(img => img.location)
 
     if(!userId) {
         res.status(400).send('필수 요소가 들어오지 않았습니다.')
     } else {
+        //이전에 image를 불러온다.
+        Posting.findOne()
+        .where('_id').equals(postingId).selected('image')
+        
         //new option을 써서 수정 후 값을 리턴값으로 받도록 한다.
-        await Posting.findByIdAndUpdate(postingId, {image: image, content: content}, {new:true}).exec()
+        await Posting.findByIdAndUpdate(postingId, {image: path, content: content}).exec()
         .then(data => {
+            //수정 전 값을 리턴값으로 받는다. 
             console.log('data', data)
             //삭제된 게시물
             if(!data.status) {
@@ -36,3 +43,7 @@ const modify = async (req, res) => {
 }
 
 export default modify;
+
+
+//게시물 삭제해야될 때 -> 삭제한 게시물 데이터 필요
+//게시물 수정할 때 -> 수정한 게시물의 정보를 보내기
--- controller/user/deleteFavorite.js
+++ controller/user/deleteFavorite.js
@@ -4,11 +4,11 @@ import Posting from '../../models/Posting.js';
 const deleteFavorite = async (req, res) => {
     const userId = req.params.id;
     const { postingId } = req.body;
+
     if(!postingId) {
         res.status(400).send('postingId를 보내주세요.')
-    } 
-
-    //userId에 맞는 doc에서 favorite 컬럼만 가져온다.
+    } else {
+           //userId에 맞는 doc에서 favorite 컬럼만 가져온다.
     await User.findById(userId).select('favorite').exec()
     .then(data => {    
         //삭제할 postingId 제외한 새 배열
@@ -32,6 +32,9 @@ const deleteFavorite = async (req, res) => {
 
     //like -1
     await Posting.findByIdAndUpdate(postingId, {$inc:{likes: -1}}).exec()
+    }
+
+ 
 }
 
 export default deleteFavorite;

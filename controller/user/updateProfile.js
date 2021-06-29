ebca54a01bfa8fc4dac51c7f19597f7b980f36db
Song WonJoon <wonjoon0730@gmail.com>
2021-06-29 14:01:46 +0900

    updateprofile created

---
+++ controller/user/updateProfile.js
@@ -0,0 +1,18 @@
+
+
+const updateProfile = async (req, res) => {
+    const { profileImg, password } = req.body
+
+    // 유저의 기존 관심사 데이터를 모두 삭제 
+    // post 요청으로 새로 들어온 관심사 카테고리 데이터를 생성 
+
+    if(true){
+        res.status(400).send('올바른 요청이 아닙니다.')
+    } else {
+
+        
+        res.status(201).send(  )
+    }
+}
+
+export default updateProfile

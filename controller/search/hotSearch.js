2e325464940a61c202a0549b497f03ecb129b710
Song WonJoon <wonjoon0730@gmail.com>
2021-07-09 19:31:26 +0900

    :sparkles: hotsearch finished

--- controller/search/hotSearch.js
+++ controller/search/hotSearch.js
@@ -1,6 +1,9 @@
+import Posting from "../../models/Posting.js";
 import Search from "../../models/Search.js";
 //
-const hotSearch = (req, res) => {
+const hotSearch = async (req, res) => {
+    //검색어가 search의 synonym의 배열에 없을경우, 새 hashtag로 create 해주기
+
     Search.find().sort('-searchcount').limit(1).then(
         (result) => {
             console.log(result[0].hashtag);
@@ -8,7 +11,7 @@ const hotSearch = (req, res) => {
                 data: result[0].hashtag,
                 id: result[0]._id
             })
-            //Search.findByIdAndUpdate({_id: result[0]._id}, {$inc: {searchcount: 1}}).then()
+            
         }
     )
     

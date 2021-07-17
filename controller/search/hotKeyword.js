e106e948a2c331631dc3807765c78a7498f31d19
hyebin <dlgpqls93@gmail.com>
2021-07-17 21:28:03 +0900

    :bulk: final comments

--- controller/search/hotKeyword.js
+++ controller/search/hotKeyword.js
@@ -1,11 +1,9 @@
 import Search from "../../models/Search.js";
 
 const hotKeyword = async (req, res) => {
-    //검색어가 search의 synonym의 배열에 없을경우, 새 hashtag로 create 해주기
-
+    // * search count가 가장 많은 keyword 찾기 *
     Search.find().sort('-searchcount').limit(1).then(
         (result) => {
-            console.log('result[0]', result[0].hashtag)
             res.status(200).send({
                 keyword: result[0].hashtag,
                 message: '인기 검색어'
